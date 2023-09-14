39bf5b7af9ae1adc96033fe5119640b94e541722
Vasilii <vesily92@gmail.com>
2023-09-15 01:31:37 +0400

    WeatherScreenSectionManager updated

--- WeatherApp/Managers/SectionManager/WeatherScreenSectionsManager.swift
+++ WeatherApp/Managers/SectionManager/WeatherScreenSectionsManager.swift
@@ -10,20 +10,17 @@ import Foundation
 /// A type that provides configured sections for collection view on ``WeatherViewController``.
 protocol IWeatherScreenSectionManager {
     
-    /// Provides view model for ``CurrentWeatherView``.
-    /// - Parameter data: The data received from network call.
-    /// - Returns: View model for ``CurrentWeatherView``.
-    func setupCurrentWith(
-        data: WeatherModel.Data
-    ) -> WeatherModel.ViewModel.Current
-    
     /// Provides configured sections for collection view on ``WeatherViewController``.
     /// - Parameter data: The data received from network call.
     /// - Returns: Array of configured sections.
-    func createSectionsWith(data: WeatherModel.Data) -> [Section]
+    func createSectionsWith(
+        location: Location,
+        weatherData: WeatherData?
+    ) -> [Section]
 }
 
 // MARK: - WeatherScreenSectionManager
+
 /// Manager that provides sections for collection view on ``WeatherViewController``.
 final class WeatherScreenSectionManager {
     
@@ -35,24 +32,23 @@ final class WeatherScreenSectionManager {
 }
 
 // MARK: - WeatherScreenSectionManager + IWeatherScreenSectionManager
+
 extension WeatherScreenSectionManager: IWeatherScreenSectionManager {
-    func setupCurrentWith(data: WeatherModel.Data) -> WeatherModel.ViewModel.Current {
-        sectionService.setupCurrentWith(data: data)
-    }
     
-    func createSectionsWith(data: WeatherModel.Data) -> [Section] {
-        guard let weather = data.weather else { return [] }
-            
-        if weather.alerts != nil {
+    func createSectionsWith(
+        location: Location,
+        weatherData: WeatherData? = nil
+    ) -> [Section] {
+        guard let weatherData = weatherData else { return [] }
+        
+        if weatherData.alerts != nil {
             return sectionService.createWeatherSections(
-                for: SectionType.WeatherScreen.allCases,
-                with: data
+                for: SectionType.WeatherScreen.allCases
             )
             
         } else {
             return sectionService.createWeatherSections(
-                for: [.hourly, .daily, .conditions, .footer],
-                with: data
+                for: [.spacer, .hourly, .daily, .conditions, .footer]
             )
         }
     }
